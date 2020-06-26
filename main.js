1c91cb084d2d3df212f3c69c4dc2773aea464db9
Sara <saracorsob@gmail.com>
2020-06-26 15:14:37 +0200

    Aggiunta file + consegna esercizio

---
+++ main.js
@@ -0,0 +1,14 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo a
+// disposizione una decina di dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+// Bonus: Creare una select con i seguenti generi: pop, rock,
+// metal e jazz. In base a cosa scegliamo nella select vedremo i
+// corrispondenti cd.
+
+// https://flynn.boolean.careers/exercises/api/array/music
+
+
+$(document).ready(function() {
+	//Code
+});
