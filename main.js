c469c1e0c5765e3f3593f6ba8a79c90b027a36b7
Sara <saracorsob@gmail.com>
2020-06-26 15:35:48 +0200

    Prima parte, senza commenti

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // disposizione una decina di dischi musicali.
 // Servendoci di handlebars stampiamo tutto a schermo.
 // In questo momento non è importante la parte grafica.
+
 // Bonus: Creare una select con i seguenti generi: pop, rock,
 // metal e jazz. In base a cosa scegliamo nella select vedremo i
 // corrispondenti cd.
@@ -10,5 +11,29 @@
 
 
 $(document).ready(function() {
-	//Code
+
+  $.ajax(
+    {
+      url: 'https://flynn.boolean.careers/exercises/api/array/music',
+      method: 'GET',
+      success: function(data) {
+        var cds = data.response;
+        console.log(cds);
+
+        for (var i = 0; i < cds.length; i++) {
+          var source = $('#cd-template').html();
+          var template = Handlebars.compile(source);
+          var context = cds[i];
+          var html = template(context);
+
+          $('.cds-container').append(html);
+          console.log(context);
+        }
+      },
+      error: function(richiesta, stato, errori) {
+        alert('ERRORE: ' + errori);
+      }
+    }
+  );
+
 });
