af39bffb4c0172a98484cba413959bfbefa77da0
Sara <saracorsob@gmail.com>
2020-06-26 15:57:20 +0200

    Aggiunta commenti js + creazione select

--- main.js
+++ main.js
@@ -12,22 +12,27 @@
 
 $(document).ready(function() {
 
+  // Faccio la chiamata di AJAX per avere una lista di brani
   $.ajax(
     {
       url: 'https://flynn.boolean.careers/exercises/api/array/music',
       method: 'GET',
       success: function(data) {
+        // Metto l'array contenente i 10 oggetti dei brani
+        // in una variabile cds
         var cds = data.response;
-        console.log(cds);
 
+        // Avvio un ciclo for per copiare i 10 oggetti nell'html
         for (var i = 0; i < cds.length; i++) {
+          // Con l'utilizzo di Handlebars copio il template
           var source = $('#cd-template').html();
           var template = Handlebars.compile(source);
+          // E completo il context con i singoli oggetti
           var context = cds[i];
           var html = template(context);
 
+          // Appendo al container i div creati
           $('.cds-container').append(html);
-          console.log(context);
         }
       },
       error: function(richiesta, stato, errori) {
